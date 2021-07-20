39975f7b53400be0e1112823d25323e7fa4df059
yuzhou42 <yu.nico.zhou@gmail.com>
2021-07-20 18:26:51 -0500

    modify build and change library version in cmake file

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 echo "Configuring and building Thirdparty/DBoW2 ..."
-
 cd Thirdparty/DBoW2
+rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake  -DCMAKE_BUILD_TYPE=Release ..
+make -j4
 
 cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
-
+rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
 
 cd ../../../
 
@@ -24,8 +24,8 @@ tar -xf ORBvoc.txt.tar.gz
 cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
-
+rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake  -DCMAKE_BUILD_TYPE=Release ..
+make -j4
--- build_ros.sh
+++ build_ros.sh
@@ -1,7 +1,8 @@
 echo "Building ROS nodes"
 
 cd Examples/ROS/ORB_SLAM3
+rm -rf build
 mkdir build
 cd build
-cmake .. -DROS_BUILD_TYPE=Release
-make -j
+cmake .. -DROS_BUILD_TYPE=Release 
+make -j4
